2b8bd0aae38c89e6424dc7bbdbbb08498010cea9
AYUSH SINGH <138702574+Ayush-Singh8521@users.noreply.github.com>
2025-05-31 11:37:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from legal_advisor_prompt import LEGAL_ADVISOR_PROMPT
 from similar_case_prompt import SIMILAR_CASE_PROMPT
 import re
 import requests
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-OPENAI_DEPLOYMENT_ENDPOINT_GPT4 = os.getenv("OPENAI_DEPLOYMENT_ENDPOINT_GPT4")
-OPENAI_DEPLOYMENT_NAME_GPT4 = os.getenv("OPENAI_DEPLOYMENT_NAME_GPT4")
-OPENAI_API_KEY_GPT4 = os.getenv("OPENAI_API_KEY_GPT4")
-OPENAI_API_VERSION = os.getenv("OPENAI_API_VERSION")
+OPENAI_API_TYPE = "azure"
+OPENAI_API_BASE = "https://bfslabopenai.openai.azure.com/"
+OPENAI_API_VERSION = "2023-05-15"
+OPENAI_MODEL_NAME_GPT4 = "gpt-4"
+OPENAI_DEPLOYMENT_NAME_GPT4 = "BFSLABAUSGPT4"
+OPENAI_API_KEY_GPT4 = "68a4beec201548f889287641f6ba9401"  # Replace with your actual API key
+OPENAI_DEPLOYMENT_ENDPOINT_GPT4 = "https://bsflabaus.openai.azure.com/"
 
 
 app = FastAPI()
